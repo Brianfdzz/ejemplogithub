f3d41d59f431fbf0ab75bbe6ad106c124a3a13a0
Brian <brianfl02@educastur.es>
2021-11-10 12:40:55 +0100

    Primmer commit

---
+++ src/pruebagiteclipse/PruebaGit.java
@@ -0,0 +1,10 @@
+package pruebagiteclipse;
+
+public class PruebaGit {
+
+	public static void main(String[] args) {
+		System.out.print("Probando git...");
+		System.out.print("Pepe es virgen");  
+	}
+
+}
